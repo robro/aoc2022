0e814c18a55a8484d2478549b3b176fe089fb786
Ian Brock <pibborando@gmail.com>
2022-12-07 17:25:02 -0800

    ✨ Add day 6 solution

---
+++ 6/6b.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def main():
+    with open('puzzle_input.txt', 'r') as input_file:
+        input_buffer = input_file.read().splitlines()[0]
+
+    marker_length = 14
+    for index in range(len(input_buffer)):
+        window = input_buffer[index : index + marker_length]
+        if len(set(window)) == marker_length:
+            print("Characters processed:", index + marker_length)
+            break
+
+if __name__ == "__main__":
+    main()
