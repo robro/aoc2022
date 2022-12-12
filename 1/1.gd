52a3326b0ab6f7378bc8403f7e4a1e568e10d1f7
Ian Brock <pibborando@gmail.com>
2022-12-11 21:34:26 -0800

    ✨ Add day 1 solutions in gdscript

---
+++ 1/1.gd
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S godot -s
+extends SceneTree
+
+func _init():
+    var input_file = File.new()
+    input_file.open("puzzle_input.txt", File.READ)
+    var input_lines = input_file.get_as_text().split('\n')
+    input_file.close()
+
+    var elf_calories = 0
+    var elves_total_calories = []
+    var index = 0
+    for line in input_lines:
+        if line:
+            elf_calories += int(line)
+            if index == input_lines.size() - 1:
+                elves_total_calories.append(elf_calories)
+        else:
+            elves_total_calories.append(elf_calories)
+            elf_calories = 0
+        index += 1
+
+    print("Most calories: ", elves_total_calories.max())
+    elves_total_calories.sort()
+    print(
+        "Top 3 combined total: ",
+        elves_total_calories.pop_back() +
+        elves_total_calories.pop_back() +
+        elves_total_calories.pop_back())
+    quit()
