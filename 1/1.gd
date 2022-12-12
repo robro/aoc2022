4c887714bd41aa5c27b9c70ab7603f1121af0303
Ian Brock <pibborando@gmail.com>
2022-12-12 09:47:15 -0800

    🔧 Simplify loop

--- 1/1.gd
+++ 1/1.gd
@@ -9,16 +9,13 @@ func _init():
 
     var elf_calories = 0
     var elves_total_calories = []
-    var index = 0
     for line in input_lines:
         if line:
             elf_calories += int(line)
-            if index == input_lines.size() - 1:
-                elves_total_calories.append(elf_calories)
         else:
             elves_total_calories.append(elf_calories)
             elf_calories = 0
-        index += 1
+    elves_total_calories.append(elf_calories)
 
     print("Most calories: ", elves_total_calories.max())
     elves_total_calories.sort()
