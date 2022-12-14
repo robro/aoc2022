5a3d876d1547484209af2e1f8b69d5120e034593
Ian Brock <pibborando@gmail.com>
2022-12-14 11:02:15 -0800

    ✨ Finish day 14 part 2 solution

--- 14/14a.py
+++ 14/14a.py
@@ -18,19 +18,19 @@ def main():
         input_lines = f.read().splitlines()
 
     min_x, max_x, max_y = getMaxBounds(input_lines)
-    # print(min_x, max_x, max_y)
 
     # generate empty cave
-    cave = [['.']*((max_x - min_x)+1) for _ in range(max_y+1)]
+    x_extend = 1
+    cave = [['.']*(max_x+1+x_extend) for _ in range(max_y+1)]
 
-    # use input to add features to cave
+    # add features to cave
     for line in input_lines:
         vertices = [eval(v) for v in line.split(' -> ')]
         for i in range(len(vertices)):
             start_x = vertices[i][0]
             start_y = vertices[i][1]
             if i == len(vertices)-1:
-                cave[start_y][start_x-min_x] = '#'
+                cave[start_y][start_x] = '#'
                 break
             end_x = vertices[i+1][0]
             end_y = vertices[i+1][1]
@@ -41,13 +41,44 @@ def main():
             for ii in range(max(len(x_values), len(y_values))):
                 x = start_x if not x_values else x_values[ii]
                 y = start_y if not y_values else y_values[ii]
-                cave[y][x-min_x] = '#'
+                cave[y][x] = '#'
 
-    # show cave
-    for row in cave:
-        for tile in row:
+    sand_origin = (500, 0)
+    cave[sand_origin[1]][sand_origin[0]] = '+'
+
+    # drop sand
+    sand_grains = 0
+    filling = True
+    while filling:
+        x, y = sand_origin
+        while True:
+            if y+1 > max_y:
+                # falling into the abyss
+                filling = False
+                break
+            if cave[y+1][x] == '.':
+                y += 1
+            elif cave[y+1][x-1] == '.':
+                y += 1
+                x -= 1
+            elif cave[y+1][x+1] == '.':
+                y += 1
+                x += 1
+            else:
+                # draw stationary sand grain
+                cave[y][x] = 'o'
+                sand_grains += 1
+                if (x, y) == sand_origin:
+                    filling = False
+                break
+
+    # draw cave
+    for row in cave[:max_y+1]:
+        for tile in row[min_x:max_x+1]:
             print(tile, end='')
         print()
+
+    print('Grains of sand:', sand_grains)
 
 if __name__ == "__main__":
     main()
